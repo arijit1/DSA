59d73fafe1c76020a5d19f2ae72f0954cb068233
ARIJIT SENGUPTA <sengupta.arijit84@gmail.com>
2022-03-16 22:13:07 +0530

    optimisation GFG min no. of jump

--- Minimum number of jumps/index.js
+++ Minimum number of jumps/index.js
@@ -2,25 +2,33 @@
 //Find the minimum number of jumps to reach the end of the array (starting from the first element). If an element is 0, then you cannot move through that element.
 //Note: Return -1 if you can't reach the end of the array.
 
-let arr = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9];
+let arr = [1, 0, 5, 8, 9, 0, 6, 7, 6, 8, 9];
 
 //1->3
 
 function jumps(arr) {
-    let jump = 0;
-    let pos = 0;
-    let des = 0;
+    let halt = 0, jump = 0, max = 0;
+    if (arr.length <= 1) {
+        return 0;
+    }
     if (arr[0] === 0) {
         return -1;
     }
-    for (let i = 0; i < n - 1; i++) {
-        des = Math.max(des, i + arr[i]);
-        if (pos === i) {
-            pos = des;
+    for (let i = 0; i < arr.length - 1; i++) {
+        max = Math.max(max, i + arr[i]);
+        if (max >= arr.length - 1) {
+            jump++;
+            return jump;
+        }
+        if (i === halt) {
+            halt = max;
             jump++;
         }
     }
-    return jump;
+    if (halt >= arr.length - 1) {
+        return jump;
+    }
+    return -1;
 }
 
 console.log(jumps(arr));
