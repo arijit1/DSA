843ed8a0001fc7c522da945c1bcbfb6f6bf7baae
ARIJIT SENGUPTA <sengupta.arijit84@gmail.com>
2022-03-29 21:36:09 +0530

    Count Pairs optimize

--- Count pairs with given sum /index.js
+++ Count pairs with given sum /index.js
@@ -11,15 +11,22 @@ function getPairsCount(arr, n, k) {
     return count;
 }
 let arr = [1, 5, 7, 1]
-console.log(getPairsCount(arr, arr.length, 6));
+//console.log(getPairsCount(arr, arr.length, 6));
 
-function getPairsCount(arr, n, k) {
+function getPairsCountEfficient(arr, n, k) {
     let map = new Map();
     let count = 0;
     for (let i = 0; i < n; i++) {
-        if (!map.has(arr[i])) {
-            map.set(arr[i],0);
+        let b = k- arr[i];
+        if(map.has(b)){
+            count += map.get(b);
         }
-        
+        if(map.has(arr[i])){
+            map.set(arr[i],map.get(arr[i])+1);
+        }else{
+            map.set(arr[i],1)
+        }
     }
+    return (count);
 }
+console.log(getPairsCountEfficient(arr, arr.length, 6));
