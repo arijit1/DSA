0512e09823e6e39905b6ed1e28a4de595a67ebe3
ARIJIT SENGUPTA <sengupta.arijit84@gmail.com>
2022-04-05 22:43:37 +0530

    time o(n), space o(n)

---
+++ Longest consecutive subsequence /index.js
@@ -0,0 +1,24 @@
+/**
+ * Given an array of positive integers. Find the length of the longest sub-sequence such that elements in the subsequence are consecutive integers, the consecutive numbers can be in any order.
+ */
+
+function findLongestConseqSubseq(arr, n) {
+    let s = new Set();
+    let ans = 0;
+    for (let i = 0; i < n; i++) {
+        s.add(arr[i]);
+    }
+    for (let i = 0; i < n; i++) {
+        if (!s.has(arr[i] - 1)) {
+            var j = arr[i];
+            while (s.has(j)) {
+                j++;
+            }
+            console.log(j);
+            ans = Math.max(ans, j - arr[i]);
+        }
+    }
+    return ans;
+}
+let arr = [2, 6, 1, 9, 4, 5, 3];
+console.log(findLongestConseqSubseq(arr, arr.length))
