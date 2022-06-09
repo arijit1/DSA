7fc4717a70f2766e59b72699331982c48ae61e18
ARIJIT SENGUPTA <sengupta.arijit84@gmail.com>
2022-06-09 17:59:31 +0530

    adjacency matrix

--- GRAPH- Create Graph/index.js
+++ GRAPH- Create Graph/index.js
@@ -1,31 +1,48 @@
-class Graph{
-    constructor(noOfVertices){
-        this.noOfVertices = noOfVertices;
-        this.AdjList = new Map();
+class Graph {
+    constructor(m) {
+        let arr= [];
+        for (var i = 0; i < m; i++) {
+            arr[i] = [];
+        }
+        this.twoDarray = arr;
     }
-    addVertex(v){
-        this.AdjList.set(v,[])
+    addEdge(v, w) {
+        this.twoDarray[v][w] = 1;
+        this.twoDarray[w][v] = 1;
     }
-    addEdge(v,w){
-        this.AdjList.get(v).push(w);
-        this.AdjList.get(w).push(v);
+    fillWithZero(){
+        for(var i=1;i<6;i++){
+            for(var j=1;j<6;j++){
+                if(this.twoDarray[i][j]!==1){
+                    this.twoDarray[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    print(){
+        console.log(this.twoDarray)
     }
 }
 let g = new Graph(6);
+g.addEdge(1,2);
+g.addEdge(3,5);
+g.addEdge(1,5);
+g.addEdge(4,1);
+g.addEdge(4,2);
+g.addEdge(4,5);
 
-let vertices = ['A','B','C','D','E','F'];
+g.fillWithZero();
 
-for(let i =0; i<vertices.length;i++){
-    g.addVertex(vertices[i])
-}
-g.addEdge('A', 'B');
-g.addEdge('A', 'D');
-g.addEdge('A', 'E');
-g.addEdge('B', 'C');
-g.addEdge('D', 'E');
-g.addEdge('E', 'F');
-g.addEdge('E', 'C');
-g.addEdge('C', 'F');
+g.print();
 
+//Output
 
-console.log(g.AdjList);
+// [
+//     [],
+//     [ <1 empty item>, 0, 1, 0, 1, 1 ],
+//     [ <1 empty item>, 1, 0, 0, 1, 0 ],
+//     [ <1 empty item>, 0, 0, 0, 0, 1 ],
+//     [ <1 empty item>, 1, 1, 0, 0, 1 ],
+//     [ <1 empty item>, 1, 0, 1, 1, 0 ]
+//   ]
