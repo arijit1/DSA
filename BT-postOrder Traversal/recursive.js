85d818fa0096c3e83342d1b72b33f9923e49710a
ARIJIT SENGUPTA <sengupta.arijit84@gmail.com>
2022-07-07 21:53:04 +0530

    BTree postOrder traversal

---
+++ BT-postOrder Traversal/recursive.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(data, left, right) {
+ *     this.data = (data===undefined ? 0 : data)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+ class Solution{
+    
+    //LNR
+
+    //LRN
+
+    postOrder(root,ans){
+        if(root == null) return ans;
+
+        this.postOrder(root.left,ans);
+        this.postOrder(root.right,ans);
+        ans.push(root.data);
+    }
+
+    postOrderTraversal(root){
+        let ans = [];
+        postOrder(root,ans);
+        return ans;
+    }
+
+}
