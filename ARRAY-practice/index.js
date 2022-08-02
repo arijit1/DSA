c7aeb108429eb558ab2fec3369f05a6218667d53
ARIJIT SENGUPTA <sengupta.arijit84@gmail.com>
2022-08-02 22:57:23 +0530

    Array leetcode 1

---
+++ ARRAY-practice/index.js
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/two-sum/
+
+let arr = [2,1,3,4] , target = 5;
+let ans = [];
+//O(n^2)
+for(let i=0;i<arr.length-1;i++){
+    for(let j=i+1;j<arr.length;j++){
+        console.log(j)
+        if(arr[i]+arr[j]===target){
+            ans.push([arr[i],arr[j]])
+        }
+    }
+}
+console.log(ans);
+
+let map = new Map();
+let ans2 = [];
+for(let p=0;p<arr.length;p++){
+    let compare = target - arr[p];
+    if(map.get(compare)){
+        ans2.push([map.get(compare),p]);
+    }else{
+        map.set(compare,p)
+    }
+}
+console.log(map)
+console.log(ans2)
