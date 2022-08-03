67de15da7216da8383857b470ba11b0e114ff0c5
ARIJIT SENGUPTA <sengupta.arijit84@gmail.com>
2022-08-03 22:08:23 +0530

    linkedlist create reverse pallindrome

--- LinkedList/index.js
+++ LinkedList/index.js
@@ -9,7 +9,8 @@ class LinkedList {
         let nodeHead = new Node(11);
         nodeHead.next = new Node(22);
         nodeHead.next.next = new Node(33);
-
+        nodeHead.next.next.next = new Node(22);
+        nodeHead.next.next.next.next = new Node(11);
         return nodeHead;
     }
     reverseNodes() {
@@ -28,7 +29,7 @@ class LinkedList {
             return curr;
         }
         let headNode = this.RecursivelyReverseNodes(curr.next);
-        console.log(curr);
+        //console.log(curr);
         let tempNode = curr.next;
         tempNode.next = curr;
         curr.next = null;
@@ -39,6 +40,28 @@ class LinkedList {
         let result = this.RecursivelyReverseNodes(node);
         console.log(result);
     }
+
+    isLinkedListPallindrome() {
+        let node = this.createNodes();
+        let slow = node, fast = node;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        let mid = slow.data; //slow reached mid point of linked list.
+        console.log('mid', mid);
+        let reverse = this.RecursivelyReverseNodes(slow.next);
+        let flag = true;
+        while(reverse!=null){
+            if(node.data !== reverse.data){
+               flag = false;
+            }
+            node = node.next;
+            reverse = reverse.next;
+        }
+        console.log(flag);
+        return flag;
+    }
 }
 let ll = new LinkedList();
-ll.callRecursiveReverseNodes();
+ll.isLinkedListPallindrome();
