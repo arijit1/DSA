24ccc139d38a31ca6e9777479ba6a8e0ad208ffa
ARIJIT SENGUPTA <sengupta.arijit84@gmail.com>
2023-11-10 21:04:36 +0530

    gfg intersection of two linkedlist

--- LinkedList/index2.js
+++ LinkedList/index2.js
@@ -8,107 +8,131 @@ class Node {
 
 class LinkedList {
     createList() {
-        // let head = new Node(1);
-        // let currentNode = head;
-        // for(let i=2;i<6;i++){
-        //     currentNode.next = new Node(i);
-        //     currentNode = currentNode.next;
-        // }
-        // return head;
-        let head = new Node(1);
-        head.next = new Node(10);
-        head.next.next = new Node(7);
-        head.next.next.next = new Node(8);
-        return head;
-    }
+        let node = new Node(1);
+        node.next = new Node(2);
+        node.next.next = new Node(3);
+        node.next.next.next = new Node(4);
+        node.next.next.next.next = new Node(5);
+        node.next.next.next.next.next = new Node(6);
 
-    printList(list) {
-        let set = [];
-        while (list.next != null) {
-            set.push(list.head);
+        return node;
+    }
+    createList2() {
+        let node = new Node(9);
+
+        return node;
+    }
+    print(list) {
+        let res = [];
+        while (list != null) {
+            res.push(list.head);
             list = list.next;
         }
-        set.push(list.head);
-        return set;
+        console.log(res);
     }
-
-    deleteNode(head, val) {
-        val=8;
-        
-        while (head !== null && head.head === val) {
-            head = head.next;
-        }
-        let curr = head, prev = head;
-        while(curr!=null){
-            if(curr.head == val){
-                prev.next = curr.next;
-            }else{
-                prev = curr;
-            }
-            curr = curr.next;
-            
-        }
-        return head;
-    }
-
-    reverseTheList(list) {
+    //reverse
+    reverse(list) {
         if (list == null || list.next == null) {
             return list;
         }
-        let listHead = this.reverseTheList(list.next);
+        let headNode = this.reverse(list.next);
 
         let temp = list.next;
         list.next = null;
         temp.next = list;
-        return listHead;
-    }
-    reverseTheListByGroup(list, k) {
-        //1->2->3->4->5
-        let prev = null, next = null, count = 1;
-        while (list != null && count != k) {
-            count++;
-            next = list.next;
-            list.next = prev;
-            prev = list;
-            list = next;
-        }
 
-        if (next != null) {
-            list.next = this.reverseTheListByGroup(next, count);
+        return headNode;
+    }
+    reverseByWhile(head) {
+        let prev = null, curr = head, next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
         return prev;
-
-
     }
-    rotate(head, k) {
-        if (k == 0) {
-            return head;
-        }
-        let count = 1, curr = head;
-        while (count < k && curr != null) {
-            curr = curr.next;
+    reverseByGroup(n, head) {
+        let prev = null, curr = head, next = null, count = 0;
+        while (curr != null && count != n) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
             count++;
         }
-        let kthNode = curr;
-        if (curr == null) {
+        if (next != null) {
+            head.next = this.reverseByGroup(count, next);
+        }
+        //prev.next = curr;
+        return prev;
+    }
+    moveLastElementToFirst(curr) {
+        if (curr == null || curr.next == null) {
             return curr;
         }
-        while (curr.next != null) {
-            curr = curr.next;
+        let head = curr;
+        let last = head, seclast = null;
+        while (last.next != null) { //1,2,3,4,5,6,7,null
+            seclast = last;
+            last = last.next;
         }
-        curr.next = head;
-        head = kthNode.next;
-        kthNode.next = null;
-        return head;
+        seclast.next = null;
+        last.next = head;
+        return last;
+    }
+
+    intersection(head1,head2){
+        let newNode , resultHead =null;
+        let curr2  = head2 ;
+        let curr1  = head1;
+        
+        while(curr1!=null && curr2!=null){
+            if(curr1.head == curr2.head){
+                if(newNode == undefined){
+                    newNode = new Node(curr1.head);
+                    resultHead = newNode;
+                    curr1 = curr1.next;
+                    curr2 = curr2.next;
+                }else{
+                    while(newNode.next!=null){
+                        newNode = newNode.next;
+                    }
+                    newNode.next = new Node(curr1.head);
+                    
+                    curr1 = curr1.next;
+                    curr2 = curr2.next;
+                }
+            }else{
+                if(curr1.head<curr2.head){
+                    curr1 = curr1.next;
+                }else{
+                    curr2 = curr2.next;
+                }
+            }
+        }
+        this.print(resultHead);
     }
 }
-let obj = new LinkedList();
-let list = obj.createList();
-console.log(obj.printList(list));
-let rev = obj.reverseTheList(list);
-console.log(obj.printList(rev));
-//let groupRev = obj.reverseTheListByGroup(list,1);
-//console.log(obj.printList(groupRev));
-//let rotate = obj.rotate(rev,3);
-let del = obj.deleteNode(rev, 10);
-console.log(del);
+let list = new LinkedList();
+let ll = list.createList();
+list.print(ll);
+let rev = list.reverse(ll);
+list.print(rev);
+let revwhile = list.reverseByWhile(rev);
+list.print(revwhile);
+let revGroup = list.reverseByGroup(2, revwhile);
+console.log('-----x-current list-x-----');
+list.print(revGroup);
+console.log('----------');
+let movelastfirst = list.moveLastElementToFirst(revGroup);
+list.print(movelastfirst);
+
+console.log("========================================");
+//---------//
+let list1 =  list.createList();
+let list2 = list.createList2();
+list.print(list1);
+list.print(list2);
+list.intersection(list1,list2);
