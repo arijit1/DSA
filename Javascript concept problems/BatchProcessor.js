a8bb4fb989204ef6b716bb6f725a7da36e0903f6
ARIJIT SENGUPTA <sengupta.arijit84@gmail.com>
2025-08-31 13:06:57 +0530

    Batch Process in JS DOM tree with requestAnimationFrame(RAF) and requestIdleCallback

--- Javascript concept problems/BatchProcessor.js
+++ Javascript concept problems/BatchProcessor.js
@@ -1,7 +1,8 @@
 //what if the requirement is to process all 1M items invisibly (no pagination), but still keep the main thread responsive?
+//with RAF -> requestAnimationFrame (RAF)
 
-// <p id="search"></p>
-//const ele = document.getElementById("search");
+/** <p id="search"></p>
+//const ele = document.getElementById("search"); */
 function process(length, batch = 100){
     let index = 0;
     
@@ -19,3 +20,39 @@ function process(length, batch = 100){
 }
 
 process(1000000,100);
+
+
+/** 
+ * What is requestIdleCallback?
+
+It’s a browser API that runs your function when the main thread is idle (not busy painting, handling input, etc.).
+
+Good for low-priority tasks: analytics, prefetching, cleanup, background loops.
+
+Browser gives you a special object deadline with:
+
+deadline.timeRemaining() → how many ms are left in this idle period.
+
+deadline.didTimeout → whether your callback ran because of a timeout.
+ */
+
+const ele = document.getElementById("search");
+
+function process_RIC(length, batch = 100) {
+  let index = 0;
+
+  function work(deadline) {
+    while (index < length && deadline.timeRemaining() > 0) {
+      ele.innerHTML = index;
+      index++;
+    }
+
+    if (index < length) {
+      requestIdleCallback(work);
+    }
+  }
+
+  requestIdleCallback(work);
+}
+
+process_RIC(10000, 100);
